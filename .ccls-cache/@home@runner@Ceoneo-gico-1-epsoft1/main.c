f750eaae4cd644a7d11212b5df9736977050f8ba
강은평 <105893403+epkang@users.noreply.github.com>
2022-05-20 02:56:42 +0000

    initial version

--- .ccls-cache/@home@runner@Ceoneo-gico-1-epsoft1/main.c
+++ .ccls-cache/@home@runner@Ceoneo-gico-1-epsoft1/main.c
@@ -1,10 +1,28 @@
-#include<stdio.h>
-
-main()
+#include <stdio.h>
+int main( ) 
 {
-  float a;
-  char b;
-  char c[10];
-  scanf("%f %c %s", &a, &b, &c);
-  printf("%.3f %c %s", a,b,c);
+  char name[21];
+  int age;
+  char code;
+  float secure;
+ 
+  printf("이름을 입력하세요 : ");
+  scanf("%s", name);
+ 
+  printf("나이를 입력하세요 : ");
+  scanf("%d", &age);
+	getchar();
+
+  printf("부서코드를 입력하세요 : ");
+  scanf("%c", &code);
+
+  printf("보안키를 입력하세요 : ");
+  scanf("%f", &secure);
+
+  printf("*************************\n");
+  printf("이름 : %s\n", name);
+  printf("나이 : %d\n", age);
+  printf("부서코드 : %c\n", code);
+  printf("보안키 : %g\n", secure);
+  printf("*************************\n");
 }
